c594a360231fa563d02568f3eb332044144d3193
Mahmoudshee <mahmoudshee8914@gmail.com>
2022-09-08 02:07:26 -0700

    All tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -entry
+ *
+ * Return: 0 after printing the function
+ */
+input main(void)
+{puts("\"programming is like building a multilingual puzzle");
+	return (o);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper gramar, but the outcome is a piece of art,\n");
+	return (o);
+}
