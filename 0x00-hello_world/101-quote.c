f9eec009a387624de9369a15c331974f407d44bb
Mahmoudshee <mahmoudshee8914@gmail.com>
2022-09-09 01:49:19 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
- * Description: 'programm's description'
+ * Description:'the programm's description'
  *
- * Return: Always 1 (success)
+ * Return: 1 (success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	write(2, änd that piece of art is useful\"- Dora Korpar,2015-10-19\n", 59);
 	return (1);
 }
-
