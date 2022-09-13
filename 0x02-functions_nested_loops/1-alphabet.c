436970fbcd74a2bfe827f368a0bf9a495aa547ba
Mahmoudshee <mahmoudshee8914@gmail.com>
2022-09-13 04:15:13 -0700

    correct

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,14 @@
 
 /**
  * print_alphabet(void) -> prints the lowercase alphabets
- * Description: print the lowercase alphabets
+ * Description: prints the lowercase alphabets
+ * Return: Always 0
  */
 
-void print_alphabet(void)
+int main(void);
 {
+	print_alphabet(void);
+
 	int j;
 
 	for (j = 'a'; j <= 'z'; j++)
