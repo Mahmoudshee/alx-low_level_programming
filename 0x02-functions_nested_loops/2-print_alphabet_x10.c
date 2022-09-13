55bf7afcf894185af59f1740e64c5bca7173bf2f
Mahmoudshee <mahmoudshee8914@gmail.com>
2022-09-13 02:38:39 -0700

    task 0-2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description:  print_alphabet_x10
+ */
+
+void print_alphabet_x10(void);
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
