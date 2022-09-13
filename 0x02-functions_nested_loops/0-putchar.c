62425b723e2ceaf2e409d83b82afe49237ebb400
Mahmoudshee <mahmoudshee8914@gmail.com>
2022-09-13 03:28:58 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * Desription: A program that prints _putchar
  * Return: Always 0
  */
-int main(void)
+int main(void)'
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
