8166476818bcf7e59136f3c4c3d9838b907f2884
Mahmoudshee <mahmoudshee8914@gmail.com>
2022-09-27 00:22:21 -0700

    crrct

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,11 +13,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int index;
-unsigned char *memory = s, value = c;
+	unsigned int index;
+	unsigned char *memory = s, value = c;
 
-for (index = 0; index < n; index++)
-memory[index] = value;
+	for (index = 0; index < n; index++)
+		memory[index] = value;
 
-return (memory);
+	return (memory);
 }
