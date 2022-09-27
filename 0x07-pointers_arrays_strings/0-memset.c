0a53ee1b021aed70779700b1c1ae364e625d87fc
Mahmoudshee <mahmoudshee8914@gmail.com>
2022-09-27 00:47:26 -0700

    prfct

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-* _memset - Fills a block of memory
+ * _memset - Fills a block of memory
  * @s: starting address
  * @b: values
  * @n: no bytes
- * Return: A pointer to the filled memory area @s.
+ * Return: Always 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = b;
+	int i = 0;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
